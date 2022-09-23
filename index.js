0ca3efd41cbc838d203c6d3468da184c11c5baf8
Vinayak Jaiswal <vinayak.kris@gmail.com>
2022-09-23 15:05:41 +0530

    implemented swagger

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const res = require('express/lib/response')
 
 const app = express()
 
+// swagger docs related
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
     res.status(200).send('<h1>Hello Worldd Fuck</h1>')
 });
-
-
 
 app.get('/api/v1/instagram', (req, res) => {
     const instaSocial = {
